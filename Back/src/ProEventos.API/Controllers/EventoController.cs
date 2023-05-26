6a8799a391010a6cc7da34973e6ca4a130350e55
dmonteiro78 <dmonteiro@gmail.com>
2023-05-26 13:26:38 -0300

    Aula 2: Dotnet 5

---
+++ Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Model;
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento
+            {
+                EventoId = 1,
+                Tema = "Angular 11 e .Net 5",
+                Local = "Fortaleza",
+                Lote = "1º Lote",
+                QtdPessoas = 250,
+                DataEvento = DateTime.Now.AddDays(2).ToString(),
+                ImagemUrl = "foto.png"
+            };
+        }
+    }
+}
